1989e6295dd417e340e9b396b5b75649e6740cda
David Björklund <david.bjorklund@gmail.com>
2013-12-26 14:37:00 +0100

    test: Assure that we get same result sa mhash for longer input

--- test/test.js
+++ test/test.js
@@ -1,4 +1,8 @@
-var test = require('tap').test
+var fs = require('fs')
+
+  , test = require('tap').test
+  , mhash = require('mhash').hash
+
   , rolling = require('../rolling')
 
 test('rolling checksum', function(t) {
@@ -35,5 +39,38 @@ test('rolling checksum with interval', function(t) {
 
   stream.write('hell')
   stream.write(new Buffer('hell'))
+  stream.end()
+})
+
+test('larger rolling checksum', function (t) {
+  var input = fs.readFileSync(__filename, { encoding: 'utf8' })
+
+    , hash = function(data) {
+        var hex = mhash('adler32', data)
+        return parseInt(hex, 16)
+      }
+
+    , expected = (function () {
+        var expected = []
+          , i
+
+        for(i = 0; i <= input.length - 10; ++i) (function(i) {
+          expected.push(hash(input.slice(i, i + 10)))
+        })(i)
+
+        return expected
+      })()
+    , actual = []
+    , stream = rolling(10)
+
+  stream.on('data', function (checksum) {
+    actual.push(checksum)
+  })
+  stream.once('end', function () {
+    t.deepEqual(actual, expected)
+    t.end()
+  })
+
+  stream.write(input)
   stream.end()
 })
